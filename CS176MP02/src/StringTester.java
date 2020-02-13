e364e1233e80eefdc9dd9612e4f8067081a4dbad
hollyyberr <s1299693@monmouth.edu>
2020-02-12 20:49:42 -0500

    strings initial commit

--- CS176MP02/src/StringTester.java
+++ CS176MP02/src/StringTester.java
@@ -4,27 +4,20 @@ public class StringTester {
 	public static void main(String[] args) {
 		//testing strings
 		Strings Tester = new Strings();
-		String arbitraryArray = "AA";
-		boolean array = false;
 
 		Tester.load();
-		String longest = " ";
-		String shortest = " ";
-		
-		array = Tester.search(arbitraryArray);
-		if (array == true)
-			System.out.println("The string " + arbitraryArray + " is in the array.");
-		else
-			System.out.println("The string " + arbitraryArray + " is not in the array.");
-		
-		Tester.findMax();
-		Tester.findMin();
-		
-		longest = Tester.findLongest();
-		System.out.println("The longest string is: " + longest);
-		
-		shortest = Tester.findShortest();
-		System.out.println("The shortest string is: " + shortest);
+
+		System.out.println("Is the string 'holly' in the array:  " + Tester.search("holly"));
+		System.out.println("The max in the lexicographic ordering is: " + Tester.findMax());
+
+		System.out.println("The min in the lexicographic ordering is: " + Tester.findMin());
+
+
+
+		System.out.println("The longest string is: " + Tester.findLongest());
+		//		
+
+		System.out.println("The shortest string is: " + Tester.findShortest());
 
 	}
 
--- CS176MP02/src/Strings.java
+++ CS176MP02/src/Strings.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Strings {
 	//create and initialize local scanner
@@ -6,63 +7,44 @@ public class Strings {
 	//create and initialize local variables
 	int count = 0;
 	final int MAX_SIZE = 100;
-	private String [] strings;
 	final String BREAK = "DONE";
 	String input;
 	String max = "";
 	String min = "";
 	int one = 1;
 	int zero = 0;
-	int shortCount = 0;
 	String multipleShortest;
+	String [] strings;
+	boolean done = false;
 
 
 
 	//creates the class 1 constructor
 	public Strings()
 	{
-		strings = new String [MAX_SIZE];
+		strings = new String[MAX_SIZE];
 	}
 
 	//Load method calling the scanner and prompts user to enter strings
 	public void load() {
 		System.out.print("Enter a series of strings or -'DONE' to stop: ");
 
-		while(scan.hasNext()) {
+		while(done == false) {
 			input = scan.next();
-			if(input.equals(BREAK)) {
+			if(input.equals("DONE")) {
+				done = true;
 				count--;
-				break;
 			}
-
 			else {
 				strings[count] = input;
-				if(count == one) {
-					if(strings[count].compareTo(strings[count - one]) == zero) {
-						max = strings[count];
-						min = strings[count - one];
-						count++;
-					}
-					else {
-						min =strings[count - one];
-						max = strings[count];
-						count++;
-					}
-				}//if
-				else if (count > one) {
-					if (strings[count].compareTo(min) < zero){
-						min = strings[count];
-					}//if
-					if (strings[count].compareTo(max) > zero){
-						max = strings[count];
-					}
-					count++;
-				}
-				else
-					count++;
+				count++;
 			}
 		}
+		//System.out.print(Arrays.toString(strings));
 	}
+
+
+
 
 	//search method to check if word is in the array
 	public boolean search(String array) {
@@ -73,18 +55,42 @@ public class Strings {
 		}
 		return exists;
 	}
+
 	//find and print max function
-	public void findMax(){
-		System.out.println("The max of this set of strings is: " + max);
+	public String findMin(){
+		for(int i = 0; i <= count; i++) {
+			if(i == zero) {
+				min = strings[i];
+			}
+			else {
+				if(strings[i].compareTo(min) < 0) {
+					min = strings[i];
+				}
+			}
+		}
+		return min;
 	}
+
 	//find and print min function
-	public void findMin() {
-		System.out.println("The min of this set of strings is: " + min); 
+	public String findMax() {
+		for(int i = 0; i <= count; i++) {
+			if(i == 0) {
+				max = strings[i];
+			}
+			else{
+				if(strings[i].compareTo(max) > 0) {
+					max = strings[i];
+				}
+			}
+		}
+		return max;
 	}
-	//method for finding the longest string
+
+
+	////method for finding the longest string
 	public String findLongest() {
-		String longest = "";
-		for(int i = zero; i < count; i++) {
+		String longest = strings[0];
+		for(int i = one; i <= count; i++) {
 			if(strings[i].length() > longest.length()) {
 				longest = strings[i];
 			}
@@ -93,43 +99,32 @@ public class Strings {
 		return longest;
 	}
 
+
 	//method for finding the shortest of the strings
 	public String findShortest() {
-		String shortest = "";
-		for(int i = zero; i < count; i++) {
-			if (i == 0) {
+		String shortest = strings[0];
+		for(int i = one; i <= count; i++) {
+			if(strings[i].length() < shortest.length()) {
 				shortest = strings[i];
-				shortCount++;
-				multipleShortest = strings[i];
 			}
-			else if (strings[i].length() < shortest.length()) {
-				shortest = strings[i];
-				count++;
-			}
-			else if (strings[i].length() == shortest.length()) {
-				multipleShortest = multipleShortest + ", and " + strings[i];
-				shortCount++;
-			}
-		}
-		if (shortCount == 1)
-			return shortest;
-		else
-			return multipleShortest;
-	}
 
-	//prints the content of the array
-	public void print() {
-		boolean print = false;
-		while(print == false) {
-			if(count >= zero) {
-				input = strings[count];
-				System.out.println(input);
-				count--;
-			}
-			else {
-				print = true;
-			}
 		}
+		return shortest;
 	}
 }
-
+////prints the content of the array
+//public void print() {
+//	boolean print = false;
+//	while(print == false) {
+//		if(count >= zero) {
+//			input = strings[count];
+//			System.out.println(input);
+//			count--;
+//		}
+//		else {
+//			print = true;
+//		}
+//	}
+//}
+//}
+//
