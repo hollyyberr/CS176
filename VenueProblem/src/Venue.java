ad0c7602245b01099efb4df8aca3c7ef566c4660
hollyyberr <s1299693@monmouth.edu>
2020-02-19 11:39:37 -0500

    initial commit of venue problem

---
+++ VenueProblem/src/Venue.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Venue {
+	private boolean[][][] allSeats;
+
+
+	public Venue(int section, int row, int seats) {
+		allSeats = new boolean[section][row][seats];
+		for(int i = 0; i < section; i++) {
+			for(int j = 0; j < row; j++) {
+				for(int k = 0; k < seats; k++) {
+					allSeats[i][j][k] = false;
+
+				}
+			}
+		}
+
+	}
+	public boolean seatLookup(int sectionLookup, int rowLookup, int seatsLookup) {
+		if(allSeats[sectionLookup-1][rowLookup-1][seatsLookup-1] = true) {
+			return false;
+		}
+
+		else {
+			allSeats[sectionLookup-1][rowLookup-1][seatsLookup-1] = true; {
+				return true;
+			}
+		}
+
+	}
+} 
---
+++ VenueProblem/src/VenueTester.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class VenueTester {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		int one = 1;
+		final int SENTINEL = 100;
+		int section = 0;
+		int row = 0;
+		int seat = 0;
+		int count = 0;
+		Venue PrudentialCenter = new Venue(4,5,10);
+		
+
+
+		while(count!=100){
+			section = rand.nextInt(4) + one;
+			row = rand.nextInt(5) + one;
+			seat = rand.nextInt(10) + one;
+			if(PrudentialCenter.seatLookup(section,row,seat) == false) {
+				System.out.println("Your seat has been awarded! Your seat location is: " + section + " " + row + " " + seat + ".");
+			}
+			else if((PrudentialCenter.seatLookup(section,row,seat) == true)) {
+				System.out.println("You did not win an awarded seat, better luck next time!");
+			}
+			count++;
+			
+		}
+
+		
+
+
+	}
+
+
+}
