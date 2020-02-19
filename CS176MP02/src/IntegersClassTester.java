cf525c3cf882823e539a39a888f3b7038823c0e8
hollyyberr <s1299693@monmouth.edu>
2020-02-19 14:47:21 -0500

    final commit

--- CS176MP02/src/IntegersClassTester.java
+++ CS176MP02/src/IntegersClassTester.java
@@ -1,23 +1,21 @@
-
-
 public class IntegersClassTester {
 
 	public static void main(String[] args) {
 		
-		IntegersClass class2test = new IntegersClass(100);
+		Integers IntegersTester = new Integers();
 
-		class2test.load();
-		class2test.search(1);
-		class2test.findMax();
-		class2test.findMin();
-		class2test.findMaxPos();
-		class2test.findMinPos();
+		IntegersTester.load();
+		IntegersTester.search(1);
+		IntegersTester.findMax();
+		IntegersTester.findMin();
+		IntegersTester.findMaxPos();
+		IntegersTester.findMinPos();
 
-		System.out.println("Was the number in the search found: " + class2test.search(5));
-		System.out.println("The maximum value in the array is: " + class2test.findMax());
-		System.out.println("The minimum value in the array is: " + class2test.findMin());
-		System.out.println("The maximum value index position is: " + class2test.findMaxPos());
-		System.out.print("The minimum value index position is: " + class2test.findMinPos());
+		System.out.println("Was the number in the search found: " + IntegersTester.search(5));
+		System.out.println("The maximum value in the array is: " + IntegersTester.findMax());
+		System.out.println("The minimum value in the array is: " + IntegersTester.findMin());
+		System.out.println("The maximum value index position is: " + IntegersTester.findMaxPos());
+		System.out.print("The minimum value index position is: " + IntegersTester.findMinPos());
 
 	}
 
